2a818d1702812c6754afef7600151f776cc6cd54
Annacarvalho-coder <annaofc0209@gmail.com>
2025-04-24 13:11:40 -0300

    Atualiza arquivos com novos dados de acessibilidade

--- servindo-api/data/resources.js
+++ servindo-api/data/resources.js
@@ -1,18 +1,8 @@
 const resources = [
   {
-    name: "Centro de Apoio à Pessoa com Deficiência - Prefeitura de SP",
-    type: "Serviço público",
-    link: "https://www.prefeitura.sp.gov.br/cidade/secretarias/pessoa_com_deficiencia/"
-  },
-  {
     name: "Cartilha da Acessibilidade – Governo Federal",
-    type: "Informação em português",
+    type: "Informação (PT-BR)",
     link: "https://www.gov.br/mdh/pt-br/acesso-a-informacao/acoes-e-programas/cartilha-de-acessibilidade"
-  },
-  {
-    name: "LibrasGov",
-    type: "Comunicação em Libras (PT-BR)",
-    link: "https://www.gov.br/educacao/libras"
   },
   {
     name: "Be My Eyes",
@@ -20,14 +10,79 @@ const resources = [
     link: "https://www.bemyeyes.com/"
   },
   {
-    name: "Inclusão SP – Guia de acessibilidade do Governo do Estado",
-    type: "Mapa e informações",
+    name: "AccessAble",
+    type: "Guia de acessibilidade urbana (EN)",
+    link: "https://www.accessable.co.uk"
+  },
+  {
+    name: "iComunica",
+    type: "App de comunicação alternativa (PT)",
+    link: "https://icomunica.org/"
+  },
+  {
+    name: "LibrasGov",
+    type: "Comunicação em Libras (PT-BR)",
+    link: "https://www.gov.br/educacao/libras"
+  },
+  {
+    name: "Inclusão SP",
+    type: "Guia e mapa de acessibilidade no estado",
     link: "https://www.inclusaosp.org.br"
   },
   {
-    name: "iComunica – App para comunicação alternativa",
-    type: "Tecnologia assistiva",
-    link: "https://icomunica.org/"
+    name: "Deaf Chat Coffee",
+    type: "Rede de cafés com atendimento em Libras",
+    link: "https://deafchatcoffee.com.br"
+  },
+  {
+    name: "Central de Intermediação em Libras – São Paulo",
+    type: "Atendimento em Libras via app",
+    link: "https://www.sinallivre.sp.gov.br"
+  },
+  {
+    name: "WCAG 2.1 – W3C",
+    type: "Padrões de acessibilidade web (EN)",
+    link: "https://www.w3.org/TR/WCAG21/"
+  },
+  {
+    name: "ProDeaf",
+    type: "App tradutor de voz/texto para Libras",
+    link: "https://www.prodeaf.net/"
+  },
+  {
+    name: "Portal da Pessoa com Deficiência – Governo de SP",
+    type: "Central de serviços e informações",
+    link: "https://www.pessoacomdeficiencia.sp.gov.br"
+  },
+  {
+    name: "Movimento Web para Todos (MWPT)",
+    type: "Acessibilidade digital",
+    link: "https://mwpt.com.br"
+  },
+  {
+    name: "Fundação Dorina Nowill",
+    type: "Projetos para pessoas com deficiência visual",
+    link: "https://www.fundacaodorina.org.br"
+  },
+  {
+    name: "Mais Diferenças",
+    type: "Educação inclusiva e cultura acessível",
+    link: "https://maisdiferencas.org.br"
+  },
+  {
+    name: "Projeto Inclui",
+    type: "Inclusão de pessoas com deficiência no mercado de trabalho",
+    link: "https://www.projetoinclui.com.br"
+  },
+  {
+    name: "Hand Talk",
+    type: "App tradutor de texto/voz para Libras (Brasil)",
+    link: "https://www.handtalk.me/br/"
+  },
+  {
+    name: "Lei Brasileira de Inclusão (Estatuto da Pessoa com Deficiência)",
+    type: "Legislação completa (PT-BR)",
+    link: "http://www.planalto.gov.br/ccivil_03/_ato2015-2018/2015/lei/l13146.htm"
   }
 ];
 
